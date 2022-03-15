1c2a6a0bdbf9d326573bef0f91a69482b07cb174
Jangebe <jangebe91@gmail.com>
2022-03-15 09:21:03 +0700

    first commit

---
+++ resources/views/beranda.blade.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>BERANDA</title>
+</head>
+<body>
+    <center><h1>Selamat Datang</h1></center>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Cupiditate accusamus labore quis totam aliquid! Quos fugit quam facere esse harum nemo dolorum, aut sunt libero, fugiat sed voluptate facilis? Possimus quaerat ipsam ducimus nemo veniam architecto, aliquam adipisci repellendus, perspiciatis, ex voluptate enim dolores obcaecati commodi expedita. Libero sunt numquam maiores sapiente nam repudiandae cum dolores alias odit corporis ullam accusamus sint facilis, aperiam itaque consequuntur similique voluptatem quam iusto beatae inventore! Ullam, alias, maxime beatae voluptates temporibus hic veniam magni a quibusdam dicta aperiam sint. Quo alias quam numquam error veritatis? Explicabo atque assumenda at voluptates accusantium. Veniam, eos!</p>
+</body>
+</html>
